d7042af7031c96395d9dbe495bf3f516c43f2733
user144731 <user144731@74-80-245-6.ic2net.net>
2018-09-18 19:37:27 -0700

    added hero tab + some functionality

---
+++ flix_demo/flix_demo/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_demo
+//
+//  Created by user144731 on 9/18/18.
+//  Copyright © 2018 Roberto Bradley. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
