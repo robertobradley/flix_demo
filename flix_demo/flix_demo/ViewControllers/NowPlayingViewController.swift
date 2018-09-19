b7c947331a35b31ca9d799a5e8048f8206eadffe
user144731 <user144731@74-80-245-6.ic2net.net>
2018-09-18 18:49:38 -0700

    pushing to be a responsible programmer

--- flix_demo/flix_demo/ViewControllers/NowPlayingViewController.swift
+++ flix_demo/flix_demo/ViewControllers/NowPlayingViewController.swift
@@ -82,6 +82,15 @@ class NowPlayingViewController: UIViewController,UITableViewDataSource {
         
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell)
+        {
+        let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
+        }
+    }
     
     
     
